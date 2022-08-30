9e025a3805262599d9d40393b75c1c253867e78d
Deepanshu Singh Negi <deepanshunegi10@gmail.com>
2022-08-30 12:42:41 +0530

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,25 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { BrowserRouter, Link} from 'react-router-dom';
 import { Button } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import './Home.css';
 import { useStateValue } from '../StateProvider';
 import { actionTypes } from '../reducer';
+import AppsIcon from '@mui/icons-material/Apps';
+import Avatar from '@mui/material/Avatar';
+import SearchIcon from '@mui/icons-material/Search';
+import MicIcon from '@mui/icons-material/Mic';
 
 function Home() {
     const [input, setInput] = useState('');
     const history = useHistory();
     const [{term}, dispatch] = useStateValue();
+    const [windowWidth, setWindowWith] = useState(window.innerWidth);
 
+    useEffect(()=>{
+        window.addEventListener('resize', ()=>{setWindowWith(window.innerWidth)})
+        console.log(window.innerWidth)
+    })
 
     const search = e => {
         e.preventDefault();
@@ -28,28 +37,51 @@ function Home() {
         <BrowserRouter>
         <div className='home'>
 
-            <div className='header'>
-                    <Link to='/gmail'>Gmail</Link>
-                    <Link to='/images'>Images</Link>
+            <div className='header' style={{display:'flex', flex:'0.05', alignItems:'center'}}>
+                    <div style={{marginBottom:'6px'}}>
+                    <a href='https://www.gmail.com'>Gmail</a>
+                    <a href='https://www.google.co.in/imghp?hl=en&ogbl'>Images</a></div>
+                    <a href=''><AppsIcon /></a>
+                    <Avatar src="https://cdn.worldvectorlogo.com/logos/react-1.svg" sx={{height:'30px', width:'30px'}}/>
+                  
             </div>
 
-            <div className='body'>
+            <div className='body' style={{display:'flex', flex:'0.8', alignItems:'center'}}>
                 <img
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png"
+                    width={windowWidth / 2}
                     alt="" />
 
             <div className="inputDiv">
                         <form className='search'>
                             <div className='search_input'>
+                                <SearchIcon style={{color:'#9aa0a6'}}/>
                                 <input value={input} onChange={e => setInput(e.target.value)} />
+                                <MicIcon style={{}}/>
                             </div>
 
                             <div className='search_buttons'>
-                                <Button type='submit' onClick={search}>Google Search</Button>
-                                <Button>I'm Feeling Lucky</Button>
+                                <Button type='submit' onClick={input?search : null}>Google Search</Button>
+                                <Button onClick={null}>I'm Feeling Lucky</Button>
                             </div>
                         </form>
                 </div>
+            </div>
+            <div style={{display:'flex', flex:'0.15', backgroundColor:'rgb(243 246 252)', flexDirection:'column'}} >
+                <div style={{marginLeft:'25px', paddingTop:'10px', display:'flex', justifyContent:'flex-start'}}>
+                    <p style={{color:'#70757a'}}>India</p>
+                </div> 
+                <div className='footer2ndOption' style={{display:'flex'}}>
+                    <a href="https://about.google/?utm_source=google-IN&utm_medium=referral&utm_campaign=hp-footer&fg=1">
+                        About
+                    </a>
+                    <a href='https://ads.google.com/intl/en_in/home/?subid=ww-ww-et-g-awa-a-g_hpafoot1_1!o2&utm_source=google.com&utm_medium=referral&utm_campaign=google_hpafooter&fg=1'>
+                        Advertising
+                    </a>
+                    <a href='https://www.google.com/intl/en_in/business/'>Business</a>
+                    <a href='https://www.google.com/search/howsearchworks/?fg=1'>How Search works</a>
+                </div>
+
             </div>
         </div>
         </BrowserRouter>
--- src/pages/secondPage.js
+++ src/pages/secondPage.js
@@ -2,7 +2,15 @@ import {useState, useEffect} from 'react';
 import { actionTypes } from '../reducer';
 import { useStateValue} from '../StateProvider';
 import './secondPage.css';
-
+import CropOriginalIcon from '@mui/icons-material/CropOriginal';
+import ArticleIcon from '@mui/icons-material/Article';
+import SmartDisplayOutlinedIcon from '@mui/icons-material/SmartDisplayOutlined';
+import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import CloseIcon from '@mui/icons-material/Close';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+import SearchIcon from '@mui/icons-material/Search';
 
 const API_KEY = 'AIzaSyDpbVaPPMHGJpp5J55Y6_t6OyEtN9zxfic';
 const SearchEngineId = 'b43f3d9eb090d4aaa';
@@ -11,12 +19,13 @@ const SearchEngineId = 'b43f3d9eb090d4aaa';
 function SecondPage(){
     const [{term}, dispatch] = useStateValue();
     const [data, setData] = useState(null);
+    
 
     useEffect(()=>{
         const fetchData = async()=>{
             fetch(
                 `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${SearchEngineId}&q=${term}`
-            ).then(res=>res.json()).then(result=>{
+            ).then(response=>response.json()).then(result=>{
                 setData(result)
             })
         }
@@ -25,29 +34,66 @@ function SecondPage(){
     console.log(data)
     return(
         
-        <div>
-        <div className='mainBody'>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png' alt='' />
-            <div>
-                <form>
-                    <input value={term} />
-                    <button type='submit'/>
-                </form>
-            </div>
-        </div>
-        <div className='searchResult'>
-                    {
-                        data?.items.map(items =>(
-                            <div className='searchItems'>
-                            <p className='Url_P'>{items.formattedUrl}</p>
-                            <a href={items.link} target='_blank'>
-                                <h3>{items.title}</h3>
-                            </a>
-                            <p className='snippet_P'>{items.snippet}</p>
-                            </div>
-                        ))
-                    }
+        <div style={{width:'100vw'}}>
+            <div className='mainBody'>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png'  alt='' />
+                <div>
+                <div className='searchBarDiv' style={{boxShadow:'0 2px 5px 1px rgb(64 60 67 / 16%)', border:'1px solid transparent'}}>
+                    <form>
+                        <input value={term} />    
+                    </form>
+               <div>  
+                    <CloseIcon style={{marginTop:'9px', marginRight:'30px', color:'grey'}} />
+                    <KeyboardVoiceIcon style={{marginTop:'9px', marginRight:'15px', color:'grey'}}/>
+                    <SearchIcon style={{marginTop:'9px', marginRight:'15px', color:'grey'}}/></div>
                 </div>
+                <div className='optionsBelowInput' style={{display:'flex', justifyContent:'space-between', paddingTop:'20px'}}>
+                        <a href='' style={{display:'flex', marginLeft: '30px', paddingRight:'20px'}}>
+                            <SearchOutlinedIcon style={{display: 'block', height: '20px', width: '20px'}}/>
+                            <p style={{fontSize:'14px'}}>All</p>
+                        </a>
+                        <a href='' style={{display:'flex', paddingRight:'25px'}}>
+                            <CropOriginalIcon style={{display: 'block', height: '20px', width: '20px'}} />
+                            <p style={{fontSize:'14px'}}>Images</p>
+                        </a>
+                        <a href='' style={{display:'flex', paddingRight:'30px'}}>
+                            <ArticleIcon style={{display: 'block', height: '20px', width: '20px'}}/>
+                            <p style={{fontSize:'14px'}}>News</p>
+                        </a>
+                        <a href='' style={{display:'flex', paddingRight:'20px'}}>
+                            <SmartDisplayOutlinedIcon style={{display: 'block', height: '20px', width: '20px'}}/>
+                            <p style={{fontSize:'14px'}}>Videos</p>
+                        </a>
+                        <a href='' style={{display:'flex', paddingRight: '20px'}}>
+                            <FmdGoodOutlinedIcon style={{display: 'block', height: '20px', width: '20px'}}/>
+                            <p style={{fontSize:'14px'}}>Maps</p>
+                        </a>
+                        <a href='' style={{display:'flex'}}>
+                            <MoreVertIcon style={{display: 'block', height: '20px', width: '20px'}}/>
+                            <p style={{fontSize:'14px'}}>More</p>
+                        </a>
+                    </div>
+                    <div id="before-appbar" style={{width:'100vw', height:'0', border:'1px 0px solid grey'}}></div>
+                </div>
+            </div>
+
+            <div className='searchResult'>
+                        {
+                            data?.items.map(items =>(
+                                <div className='searchItems'>
+                                <div style={{display:'flex'}}>
+                                <p className='Url_P' style={{margin:0}}>{items.formattedUrl}</p>
+                                <MoreVertIcon style={{ paddingLeft:'15px', height:'17px', width:'17px', paddingTop:'3px' }} color='grey'/>
+                                </div>
+                                <a href={items.link} target='_blank'>
+                                <p style={{margin:0, padding:0, display:'inline-block', fontSize:'1.3rem', fontWeight:'450'}}>{items.title}</p>
+                                </a>
+                                <p className='snippet_P' style={{margin:0}}>{items.snippet}</p>
+                                </div>
+                            ))
+                        }
+            </div>
+            
         </div>
     );
 }
